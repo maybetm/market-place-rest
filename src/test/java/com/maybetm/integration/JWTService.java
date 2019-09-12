1b2f006c0727110b9c3a3548504adc89b8c94e8a
zebzeev-sv <sergey.zebzeev@gmail.com>
2019-09-13 01:29:29 +0500

    task/11092019-integration-tests

---
+++ src/test/java/com/maybetm/integration/JWTService.java
@@ -0,0 +1,19 @@
+package com.maybetm.integration;
+
+import org.junit.Test;
+
+/**
+ * Тестирование механизма создания и разбора токена.
+ *
+ * @author zebzeev-sv
+ * @version 26.07.2019 19:27
+ */
+public class JWTService extends AITest
+{
+
+  @Test
+	public void tesT() {
+  	System.out.println("sdasdasd");
+	}
+
+}
